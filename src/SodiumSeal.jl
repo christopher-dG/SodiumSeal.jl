d4a14dacad930f38bcbf637659890e8b6f785141
Chris de Graaf <chrisadegraaf@gmail.com>
2020-02-18 09:29:05 +0700

    Add libsodium dependency

--- src/SodiumSeal.jl
+++ src/SodiumSeal.jl
@@ -1,5 +1,16 @@
 module SodiumSeal
 
-# Write your package code here.
+@static if VERSION >= v"1.3"
+    using libsodium_jll: libsodium
+else
+    let deps = joinpath(@__DIR__, "..", "deps", "deps.jl")
+        isfile(deps) || error(
+            "SodiumSeal is not installed properly. ",
+            """Run Pkg.build("SodiumSeal"), restart Julia and try again.""",
+        )
+        include(deps)
+    end
+end
+
 
 end
