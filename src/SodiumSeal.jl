ee66456054c614114057eb4ed038099eeb21f475
Chris de Graaf <me@cdg.dev>
2020-12-30 13:16:45 -0600

    Only support 1.3

--- src/SodiumSeal.jl
+++ src/SodiumSeal.jl
@@ -1,16 +1,6 @@
 module SodiumSeal
 
-@static if VERSION >= v"1.3"
-    using libsodium_jll: libsodium
-else
-    let deps = joinpath(@__DIR__, "..", "deps", "deps.jl")
-        isfile(deps) || error(
-            "SodiumSeal is not installed properly. ",
-            """Run Pkg.build("SodiumSeal"), restart Julia and try again.""",
-        )
-        include(deps)
-    end
-end
+using libsodium_jll: libsodium
 
 export
     KeyPair,
